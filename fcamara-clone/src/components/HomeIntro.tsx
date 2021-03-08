1aae5cadd83b2df85db46738787ada1ee63f9b12
Diego Apolinário <dieego.apolinario@gmail.com>
2021-03-08 18:54:07 -0300

    update

--- fcamara-clone/src/components/HomeIntro.tsx
+++ fcamara-clone/src/components/HomeIntro.tsx
@@ -1,12 +1,10 @@
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
-import { Icon, InlineIcon } from '@iconify/react';
-import facebookOutline from '@iconify-icons/eva/facebook-outline';
-import linkedinOutline from '@iconify-icons/eva/linkedin-outline';
-import instagramOutlined from '@iconify-icons/ant-design/instagram-outlined';
-import youtubeIcon from '@iconify-icons/feather/youtube';
-import glassdoorIcon from '@iconify-icons/simple-icons/glassdoor';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 export function HomeIntro(){
   return(
@@ -20,25 +18,23 @@ export function HomeIntro(){
           <a href="#"><strong>programa de formação</strong></a>
           <a href="#"><strong>blog</strong></a>
           <a href="#"><strong>contato</strong></a>
-          <a href="#"><strong>PT / EN</strong></a>
+          <a href="#"><strong>PT<span> / </span></strong></a>
+          <a href="#"><strong>EN</strong></a>
       </header>
-      <nav>
+      <div className="navSocial">
         <a href="https://www.facebook.com/grupo.fcamara/">
-          <Icon icon={facebookOutline} style={{fontSize: "25px", color: "#fa5c23"}} />
+          <FacebookIcon />
         </a>
         <a href="https://www.linkedin.com/company/fcamara-consulting-&-training/">
-          <Icon icon={linkedinOutline} style={{fontSize: "25px", color: "#fa5c23"}} />
+          <LinkedInIcon />
         </a>
         <a href="https://www.instagram.com/grupo.fcamara/">
-          <Icon icon={instagramOutlined} style={{fontSize: "25px", color: "#fa5c23"}} />
+          <InstagramIcon />
         </a>
         <a href="https://www.youtube.com/channel/UCHsQ04xha1YKv48dSzQFlpw">
-          <Icon icon={youtubeIcon} style={{fontSize: "25px", color: "#fa5c23"}} />
+          <YouTubeIcon />
         </a>
-        <a href="https://www.glassdoor.com.br/Avalia%C3%A7%C3%B5es/Grupo-FCamara-Avalia%C3%A7%C3%B5es-E668818.htm">
-          <Icon icon={glassdoorIcon} style={{fontSize: "20px", color: "#fa5c23"}} />
-        </a>
-      </nav>
+      </div>
       <section className="sec">
         <img src="https://www.fcamara.com.br/images/home/home-intro.png" alt="logoIntro" className="logoIntro"/>
         <h3>INOVAÇÃO E TECNOLOGIA PARA PROMOVER</h3>
--- fcamara-clone/src/components/HomeParceiros.tsx
+++ fcamara-clone/src/components/HomeParceiros.tsx
@@ -1,4 +1,8 @@
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
 export function HomeParceiros(){
+
   return(
     <div className="containerParceiros">
       <div className="conteudo">
@@ -16,6 +20,24 @@ export function HomeParceiros(){
         <span>..</span>
         <span>..</span>
         <span>x</span>
+      </div>
+      <div className="empresas">
+        <div className="movieRowLeft">
+          <NavigateBeforeIcon style={{fontSize: 50, color: "#FE4400"}} />
+        </div>
+        <div className="empresasRow">
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-01.png" alt="Riachuelo"/></span>
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-02.png" alt="ViaVarejo"/></span>
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-03.png" alt="Walmart"/></span>
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-04.png" alt="Dasa"/></span>
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-05.png" alt="Linx"/></span>
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-06.png" alt="Sodexo"/></span>
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-07.png" alt="Movida"/></span>
+          <span><img className="empresasLogo" src="https://www.fcamara.com.br/images/home/logos-clientes/logo-08.png" alt="Drogasil"/></span>
+        </div>
+        <div className="movieRowRight">
+          <NavigateNextIcon style={{fontSize: 50, color: "#FE4400"}} />
+        </div>
       </div>
     </div>
   )
