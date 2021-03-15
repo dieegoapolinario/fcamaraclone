cc9fa87e96d5b279abd7fb2115750c328c959a78
Diego Apolinário <dieego.apolinario@gmail.com>
2021-03-15 19:31:29 -0300

    update css

--- fcamara-clone/src/pages/index.tsx
+++ fcamara-clone/src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-
 import styles from '../styles/pages/Home.module.css';
 
 import { HomeIntro } from "../components/HomeIntro";
@@ -9,12 +7,7 @@ import { HomeServices } from '../components/HomeServices';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>        
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&family=Ubuntu:wght@500&display=swap" rel="stylesheet"></link>
-      </Head>
-
       <HomeIntro />
-      </div>
+    </div>
   )
 }
